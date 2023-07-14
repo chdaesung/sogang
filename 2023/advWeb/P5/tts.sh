e604cb43dbb42e3b2278cfa846081e1c2d4ec889
chdaesung <39229461+chdaesung@users.noreply.github.com>
2023-07-14 21:22:17 +0900

    Create tts.sh

---
+++ 2023/advWeb/P5/tts.sh
@@ -0,0 +1,1 @@
+curl -X GET -u "apikey:<your_apikey>" --output "korean.wav" "<your_tts_url>/v1/synthesize?accept=audio%2Fwav&text=%ED%8C%8C%EC%9D%BC%20%EB%B3%B4%EB%83%85%EB%8B%88%EB%8B%A4&voice=ko-KR_JinV3Voice"
